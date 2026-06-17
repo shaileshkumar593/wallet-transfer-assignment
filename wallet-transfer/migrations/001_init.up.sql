e39f06df31bf78d6710851dbe82752b46cd48b0c
Shaileshkumar593 <shaileshkumar593@gmail.com>
2026-06-17 14:56:25 +0530

    completed the code

--- wallet-transfer/migrations/001_init.up.sql
+++ wallet-transfer/migrations/001_init.up.sql
@@ -1,86 +1,32 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE wallets (
     id UUID PRIMARY KEY,
-    balance BIGINT NOT NULL CHECK(balance >= 0),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TYPE transfer_status AS ENUM (
-    'PENDING',
-    'PROCESSED',
-    'FAILED'
+    balance BIGINT NOT NULL CHECK (balance >= 0)
 );
 
 CREATE TABLE transfers (
     id UUID PRIMARY KEY,
-
-    idempotency_key VARCHAR(255)
-    UNIQUE NOT NULL,
-
-    from_wallet_id UUID NOT NULL
-    REFERENCES wallets(id),
-
-    to_wallet_id UUID NOT NULL
-    REFERENCES wallets(id),
-
-    amount BIGINT NOT NULL
-    CHECK(amount > 0),
-
-    status transfer_status NOT NULL,
-
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TYPE ledger_type AS ENUM (
-    'DEBIT',
-    'CREDIT'
+    idempotency_key TEXT UNIQUE NOT NULL,
+    from_wallet_id UUID NOT NULL,
+    to_wallet_id UUID NOT NULL,
+    amount BIGINT NOT NULL CHECK (amount > 0),
+    status TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE ledger_entries (
     id UUID PRIMARY KEY,
-
-    transfer_id UUID NOT NULL
-    REFERENCES transfers(id),
-
-    wallet_id UUID NOT NULL
-    REFERENCES wallets(id),
-
-    entry_type ledger_type NOT NULL,
-
+    transfer_id UUID NOT NULL REFERENCES transfers(id),
+    wallet_id UUID NOT NULL,
+    type TEXT NOT NULL,
     amount BIGINT NOT NULL
-    CHECK(amount > 0),
-
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE idempotency_records (
-
-    idempotency_key VARCHAR(255)
-    PRIMARY KEY,
-
+    idempotency_key TEXT PRIMARY KEY,
     transfer_id UUID NOT NULL,
-
-    response JSONB NOT NULL,
-
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    response JSONB NOT NULL
 );
 
-CREATE INDEX idx_wallet_balance
-ON wallets(balance);
-
-CREATE INDEX idx_transfer_wallets
-ON transfers(
-    from_wallet_id,
-    to_wallet_id
-);
-
-CREATE INDEX idx_ledger_transfer
-ON ledger_entries(
-    transfer_id
-);
-
-CREATE UNIQUE INDEX idx_idempotency_key
-ON idempotency_records(idempotency_key);
+CREATE INDEX idx_wallets_id ON wallets(id);
+CREATE INDEX idx_transfers_wallet ON transfers(from_wallet_id, to_wallet_id);
+CREATE INDEX idx_ledger_transfer ON ledger_entries(transfer_id);
